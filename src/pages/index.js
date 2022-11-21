76b7ca46f2bc7eb72614549150e0a7588e4e36ff
captainflight <lohithmanthena22@gmail.com>
2022-11-21 12:43:46 -0500

    Beta 2.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Ye ⏱️
+            Docusaurus 🙉
           </Link>
         </div>
       </div>
