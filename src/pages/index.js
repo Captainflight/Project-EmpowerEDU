65cb3ac9888f07fa43cfe0ab25e5298ff3c34078
captainflight <lohithmanthena22@gmail.com>
2022-11-23 11:16:53 -0500

    Name change

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Yello
+            Tutorial
           </Link>
         </div>
       </div>
